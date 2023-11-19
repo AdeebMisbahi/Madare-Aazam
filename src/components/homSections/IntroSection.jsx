dc905c48046598709eb8fd12318137f6936e3dd6
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-11-19 11:18:20 +0530

    working

--- src/components/homSections/IntroSection.jsx
+++ src/components/homSections/IntroSection.jsx
@@ -5,23 +5,32 @@ const IntroSection = () => {
     <>
        <div className=''>
         <div className='mx-[5vw] rounded-md my-10 bg-[#f7f7f7]'>
-            <div className='relative grid grid-cols-2 gap-12'>
+            <div className='p-4 relative grid grid-cols-2 gap-12'>
             <div>
             <img className='rounded-lg' src="https://scontent.fbek1-1.fna.fbcdn.net/v/t1.6435-9/51156315_840789886271945_5781479331393961984_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=FUF7JmyGw1MAX8M1h3i&_nc_ht=scontent.fbek1-1.fna&oh=00_AfCEIFsjZo6nGcShHlRoOqcS30pdQrMKKYGs6WeWd_WRJQ&oe=6580D833" alt="" />
            </div>
            <div className='flex flex-col justify-center gap-2  items-start pr-5'>
-            <h1 className='font-matter-s text-3xl'>
-                Zinda Shah Madar: The Living Saint
+            <h1 className='leading-tight lg:leading-relaxed font-matter-b text-3xl md:text-4xl'>
+                Zinda Shah Madar: The Living Sufi Saint
             </h1>
-            <p className='font-matter-r '>
-            Zinda Shah Madar, also known as Qutb-ul-Madar, holds a revered place in Sufi mysticism and Islamic spirituality. Born in the 11th century, he is considered a prominent Sufi saint and a symbol of divine love and guidance. The title "Zinda Shah" translates to "Living King," signifying his spiritual vitality and eternal presence in the hearts of his followers.
-            </p>
-
+            <p className='py-4 leading-relaxed lg:leading-loose tracking-tight font-merienda-s text-xl md:text-2xl text-gray-400'>
+            <span className='hidden sm:inline'> Sayed Badiuddin Zinda Shah Madar is </span>
+            <span className=' hidden md:inline'>known as Qutub-ul-Madar, </span>
+            <span className=' hidden lg:inline'>Qutb-ul-Aqtab, Qutb-ul-Madar, Fard-ul-Afrad, Qutb-e-Wahdat among the group of Auliya while the common people call him as Madare Alam, Madare-Do-Jahan, Madar-ul-Alameen, Zinda Madar</span>
+           </p> 
            </div>
             </div>
-            <div>
-            <p className='font-matter-r '>
-            Zinda Shah Madar, also known as Qutb-ul-Madar, holds a revered place in Sufi mysticism and Islamic spirituality. Born in the 11th century, he is considered a prominent Sufi saint and a symbol of divine love and guidance. The title "Zinda Shah" translates to "Living King," signifying his spiritual vitality and eternal presence in the hearts of his followers.
+            <div className=''>
+                <p className='p-4 leading-relaxed lg:leading-loose tracking-tight font-merienda-s text-xl md:text-2xl text-gray-400'>
+            <span className='inline sm:hidden'> Sayed Badiuddin Zinda Shah Madar is </span>
+            <span className=' inline md:hidden'>known as Qutub-ul-Madar, </span>
+            <span className=' inline lg:hidden'>Qutb-ul-Aqtab, Qutb-ul-Madar, Fard-ul-Afrad, Qutb-e-Wahdat among the group of Auliya while the common people call him as Madare Alam, Madare-Do-Jahan, Madar-ul-Alameen, </span>
+            <span>
+            Sayed Badiuddin Zinda Shah Madar, born on the auspicious day of 1st Shawwal 242 AD (5th February 856 CE) in Chinar, Aleppo, Syria, emerged from a lineage deeply connected to the Noble Prophet (SAWS). His father, Sayyid Qudwatuddin Ali al-Halabi, and mother, Sayyida Fatima Sania (Bibi Hajira), bestowed upon him a heritage that intertwined the noble lines of Imam Hasan and Imam Hussain, making him a distinguished Hasani-Hussaini Sayyid. <br />
+            <span>
+            In tracing his family roots, Sayed Badiuddin proudly declared his ancestry: "I hail from Aleppo. My name is Badiuddin. I am Hasani from my mother's side and Hussaini from my father's side, and thus my grandfather is Mustafa, the king of both the worlds and the most praised." This illustrious lineage serves as a testament to his esteemed heritage, further enriching the spiritual legacy of Zinda Shah Madar.
+            </span>
+            </span>
             </p>
             </div>
             
