a79bf7b1c34c8941c66faffc45d296795a29eedb
AdeebMisbahi1 <130211733+AdeebMisbahi@users.noreply.github.com>
2023-11-23 01:22:52 +0530

    h

--- src/components/homSections/LandingSection.jsx
+++ src/components/homSections/LandingSection.jsx
@@ -6,25 +6,20 @@ import HeaderPic2 from '../../assets/images/MadareAzam4.jpg'
 import { Link } from 'react-router-dom';
 
 const LandingSection = () => {
-  const styles = {
-    background: `url(${HeaderPic2}) center/cover`,
-   
-  }
   return (
 
     <div className=''>
       <div className='relative grid grid-cols-4 gap-x-2 md:grid-cols-12'>
-      <div className='col-span-full lg:mb-0 md:col-span-7 md:col-start-6 lg:px-0 shadow-md h-[70vw] md:h-[50vw]'>
+      <div className='col-span-full lg:mb-0 md:col-span-7 md:col-start-6 lg:px-0 shadow-md h-[100vw] md:h-[50vw]'>
           <img 
             className='h-full rounded-lg w-full object-cover object-center '
             src={HeaderPic}
             alt="Okll"
           />
         </div>
-        <div className='col-span-full pt-6 md:col-start-1 md:row-start-1 md:flex md:h-full md:flex-col md:col-span-5 h-screen' style={styles}>
-          <div className='flex flex-auto flex-col items-start '>
+        <div className='col-span-full md:col-start-2 md:row-start-2'><div className='flex flex-auto flex-col items-start '>
 
-          {/* <h1 className='text-3xl md:text-4xl font-matter-r'><span className='font-matter-r leading-relaxed tracking-wide'>Dargah Shareef</span> <span className='font-matter-b leading-relaxed tracking-wide'>Hazrat Sayyed Badiuddin Zinda Shah Madar <span className='text-xl'>(R.A)</span></span></h1> */}
+          <h1 className='text-3xl md:text-4xl font-matter-r'><span className='font-matter-r leading-relaxed tracking-wide'>Dargah Shareef</span> <span className='font-matter-b leading-relaxed tracking-wide'>Hazrat Sayyed Badiuddin Zinda Shah Madar <span className='text-xl'>(R.A)</span></span></h1>
           <Link className='inline-flex justify-center items-center'>
               <div className='relative inline-flex flex-none justify-center items-center'>
              <div className='absolute text-gray-200 '>{SVG_CIRCLE}</div> 
@@ -33,7 +28,9 @@ const LandingSection = () => {
            </div>
            <span className='tracking-tight text-xl font-matter-m '>Learn More About Zinda Shah Madar</span>
           </Link>
-          </div>
+          </div></div>
+        <div className='col-span-full md:col-start-1 md:row-start-1 md:flex md:flex-col md:col-span-5 h-[100vw] md:h-[50vw]'>
+            <img className='h-full w-full object-cover object-center' src={HeaderPic2} alt="" />
           </div>
       
       </div>
